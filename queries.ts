558e06e50e61ab597a38b30a326e6a83f7f0a918
collinsell <crsell@purdue.edu>
2023-04-16 15:08:42 -0400

    queries draft

---
+++ queries.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env tsc
+import fetch from 'node-fetch';
+
+interface Package
+{
+    name: string;
+    version: string;
+    readme: string;
+}
+
+async function fetch_pkgdir(): Promise<Package[]>
+{
+    const url = 'https://registry.npmjs.com/-/v1/search?text=';
+    const pageSize = 250;
+    let page = 0;
+    let packages: Package[] = [];
+    while(true)
+    {
+        const response = await fetch(`${url}&size=${pageSize}&from=${page * pageSize}`);
+        const json = await response.json();
+        if (json.objects.length === 0) 
+        {
+            break;
+        }
+        packages = [...packages, ...json.objects.map((obj: any) => ({ name: obj.package.name, version: obj.package.version }))];
+        page++;
+    }
+    return packages;
+}
+
+async function regex_search(regex: RegExp): Promise<Package[]>
+{
+    const url = 'https://registry.npmjs.com/-/v1/search?text=';
+    const pageSize = 250;
+    let page = 0;
+    let packages: Package[] = [];
+    while (true) {
+        const response = await fetch(`${url}&size=${pageSize}&from=${page * pageSize}`);
+        const json = await response.json();
+        if (json.objects.length === 0) {
+            break;
+        }
+        const pagePackages: Package[] = await Promise.all(json.objects.map(async (obj: any) => {
+            const name = obj.package.name;
+            const version = obj.package.version;
+            const readmeResponse = await fetch(`https://registry.npmjs.com/${name}/${version}`);
+            const readmeJson = await readmeResponse.json();
+            const readme = readmeJson.readme;
+            return { name, version, readme };
+        }));
+        packages = [...packages, ...pagePackages.filter(pkg => pkg.name.match(regex) || (pkg.readme && pkg.readme.match(regex)))];
+        page++;
+    }
+    return packages;
+}
+    
