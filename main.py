e3ea401f339f3aa420cbd8c8243732e4e34d511a
lizziesarah <102872411+lizziesarah@users.noreply.github.com>
2023-04-28 13:19:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import request
-import git
+import gitpy
 import base64
+import git
+from google.cloud import storage
+
 
 app = Flask(__name__)
 
@@ -11,10 +14,27 @@ def encode_base_64(message):
     message_in_bytes = message.encode('ascii')
     message_in_base_64 = base64.b64encode(message_in_bytes)
     return  message_in_base_64
+
 def decode_base_64(message_in_base_64):
     message_in_bytes = message_in_base_64.encode('ascii')
     message = base64.b64decode(message_in_bytes)
     return message
+
+def getName(url):
+    slash_count = 0
+    index = 0
+    for letter in url:
+        index += 1
+        if letter == "/":
+            slash_count += 1
+        if slash_count == 2:
+            break
+    return url[index:]
+
+def bucket_init():
+    storage_client = storage.Client()
+    bucket_name = "package_storage"
+    bucket = storage_client.create_bucket(bucket_name)
 
 @app.route('/package', methods=['POST'])
 def create_package(token):
@@ -25,6 +45,7 @@ def create_package(token):
     query_response = ""
     rating = 0
     id = 0
+    name = ""
     if query_response == None:
         # add the following to the database
         # metadata: name, version, id
@@ -34,16 +55,26 @@ def create_package(token):
         if rating >= 0.5:
             for header in request.headers:
                 if header == 'URL':
-                    git.clone(request.args.get('URL'))
+                    url = request.args.get('URL')
+                    gitpy.clone(url)
+                    name = getName(url)
                     break
                 if header == 'Content':
                     package_contents = decode_base_64(request.args.get('Content'))
                     url = package_contents['homepage']
-                    git.clone(url)
+                    gitpy.clone(url)
+                    request.args.get('URL')
+                    break
 
             if len(ids) > 0:
                 id = ids[len(ids) - 1] + 1
                 # write id and name to database
+                storage_client = storage.Client()
+                bucket = storage_client.bucket('package_storage')
+                # blob name your_bucket_name/path_in_gcs
+                blob = bucket.blob('')
+                with blob.open('w') as file:
+                    file.write([id, name])
         return id, "Success. Check the ID in the returned metadata for the official ID.", 201
     elif query_response != None:
         return "-1", "Package exists already.", 409
@@ -56,32 +87,30 @@ def create_package(token):
     else:
         return "-1", "Package is not uploaded due to the disqualified rating.", 424
 
-@app.route('/package/byName/{name}', methods=['GET', 'DELETE'])
-def get_history_of_package(token):
-    name = request.args.get('name')
-    query = ""
-    query_response = ""
-    history = ""
-    if query_response == "":
-        return query_response, 200
-    elif name == None or token == None:
-        # also check if token is invalid
-        return "There is missing field(s) in the PackageData/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.", 400
-    elif query_response == "":
-        return "No such package", 404
-    else:
-        return "unexpected error", 500
+@app.route('/package/byName/{name}', methods=['DELETE'])
 def delete_package(token):
     # use query commands to delete package
     # check return statement of query
     name = request.args.get('name')
     query = ""
-    query_response = ""
-    if query_response == "":
+
+    query_response = 0
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('package_storage')
+    # blob name your_bucket_name/path_in_gcs
+    blob = bucket.blob('')
+    with blob.open('r') as file:
+        lines = file.readlines()
+    with blob.open('w') as file:
+        file.truncate()
+        for line in lines:
+            if line[1] == request.args.get('name'):
+                query_response = 1
+            else:
+                file.write(line)
+
+    if query_response == 0:
         return "Package is deleted", 200
-    elif name == None or token == None:
-        # also check if token is invalid
-        return "There is missing field(s) in the PackageData/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.", 400
     if query_response == "":
         return "Package does not exist", 404
     return
@@ -89,20 +118,37 @@ def delete_package(token):
 @app.route('/package/{id}', methods=['GET', 'DELETE', 'PUT'])
 def get_package_by_ID():
     ident = request.args.get('id')
-
-    return
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('package_storage')
+    # blob name your_bucket_name/path_in_gcs
+    blob = bucket.blob('')
+    with blob.open('r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if lines[0] == ident:
+                return line
+    return 404, "Package does not exist"
 
 def delete_package_by_ID(token):
+    query_response = 0
     ident = request.args.get('id')
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('package_storage')
+    # blob name your_bucket_name/path_in_gcs
+    blob = bucket.blob('')
+    with blob.open('r') as file:
+        lines = file.readlines()
+    with blob.open('w') as file:
+        file.truncate()
+        for line in lines:
+            if line[0] == ident:
+                query_response = 1
+            else:
+                file.write(line)
     if ident not in ids:
         return "Package does not exist", 404
-    query = ""
-    query_response = ""
-    if query_response == "":
+    if query_response == 0:
         return "Package is deleted", 200
-    elif ident is None or token is None:
-        # also check if token is invalid
-        return "There is missing field(s) in the PackageData/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.", 400
     if query_response == "":
         return "Package does not exist", 404
     return
@@ -117,14 +163,20 @@ def update_package():
         # delete package from database
         for header in request.headers:
             if header == 'URL':
-                git.clone(request.args.get('URL'))
+                repo = git.Repo.clone_from(request.args.get('URL'))
+                repo.git.checkout('b', request.args.get('Version'))
+                # write back to database
+                #gitpy.clone(request.args.get('URL'))
                 break
             if header == 'Content':
                 package_contents = decode_base_64(request.args.get('Content'))
                 url = package_contents['homepage']
-                git.clone(url)
+                repo = git.Repo.clone_from(url)
+                repo.git.checkout('b', request.args.get('Version'))
+                # write back to database
+                break
 
-            git.checkout('b', request.args.get('Version'))
+
         return "Version is updated", 200
     # return "There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.", 400
     # return "Package does not exist", 404
