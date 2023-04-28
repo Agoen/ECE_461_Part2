cda50eaa289784ee9554c69b1b44692f1294137c
Collin Ronald Sell <crsell@eceprog1.ecn.purdue.edu>
2023-04-28 16:22:54 -0400

    queries

--- src/install.js
+++ src/install.js
@@ -1,14 +1,33 @@
 "use strict";
-exports.__esModule = true;
-var fs = require("fs");
-var readline = require('readline');
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const readline = require('readline');
 var json = require('../package.json'); // Package json as a json object
 var dependencies = json.dependencies; // Dependencies as a json object
 var count = Object.keys(dependencies).length; // Number of dependencies in package.json
-var rl = readline.createInterface({
-    input: fs.createReadStream('src/license.txt')
+const rl = readline.createInterface({
+    input: fs.createReadStream('src/license.txt'),
 });
-rl.on('line', function (line) {
+rl.on('line', (line) => {
     count += 1;
 });
 console.log(count + ' dependencies installed.');
